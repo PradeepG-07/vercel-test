6c4932c7a99b36453865458e4ddf34a1d197b2ee
Pradeep <pradeepubuntuvps@gmail.com>
2024-02-18 00:15:14 +0530

    add new route hello server.js

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,9 @@ const app=express();
 app.get("/",(req,res)=>{
     res.status(200).send("hello");
 })
+app.get("/hello",(req,res)=>{
+    res.status(200).send("hello page");
+})
 app.listen(4000,()=>{
     console.log("server started");
 })
